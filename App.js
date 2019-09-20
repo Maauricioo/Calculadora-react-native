871c5addc474c93cb661ab79211a81d9f1b1c5bb
Maauricioo <mauriciosilvasouza33@gmail.com>
2019-09-20 15:15:14 -0300

    Implementando restante da lógica

--- App.js
+++ App.js
@@ -37,7 +37,15 @@ export default class App extends Component {
   }
 
   setOperacao = operacao => {
-
+    if(this.state.current === 0){
+      this.setState({operation, current: 1, clearDisplay: true})
+    } else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try{ 
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      }
+    }
   }
 
   render() {
